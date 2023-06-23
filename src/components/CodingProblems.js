a1c918f6458010a46ac7da1da71f646859278197
yablony <yablonskaya.yuliya@gmail.com>
2023-06-23 16:52:31 +0930

    Added code inside the CodingProblems.js to display a random problem and a section for user input

--- src/components/CodingProblems.js
+++ src/components/CodingProblems.js
@@ -2,8 +2,19 @@ import { useState } from 'react';
 import { easyLevelProblems, easyLevelSolutions, mediumLevelProblems, mediumLevelSolutions, hardLevelProblems, hardLevelSolutions } from './data/CodingProblemsData';
 
 function CodingProblems() {
+    const difficultyLevel = 'easy';
+
+    let randomProblemIndex = Math.floor(Math.random()* easyLevelProblems.length);
+
+    let randomProblem = easyLevelProblems[randomProblemIndex];
+
     return (
-        <p>Hello</p>
+        <section className="coding-problem">
+            <p>{randomProblem}</p>
+            <label>Type your answer below:</label>
+            <input type="text" />
+            <button>Feed the code</button>
+        </section>
     )
 }
 
