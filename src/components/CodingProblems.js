dcb31a9bf01874efc2d404931ef3662d93253007
yablony <yablonskaya.yuliya@gmail.com>
2023-06-23 18:54:18 +0930

    Added CompareSolutions function to the CodingProblems component

--- src/components/CodingProblems.js
+++ src/components/CodingProblems.js
@@ -3,18 +3,39 @@ import { easyLevelProblems, easyLevelSolutions, mediumLevelProblems, mediumLevel
 
 function CodingProblems() {
     const difficultyLevel = 'easy';
+    const result = '';
 
     let randomProblemIndex = Math.floor(Math.random()* easyLevelProblems.length);
 
     let randomProblem = easyLevelProblems[randomProblemIndex];
 
+    const userInput = () => {
+        
+    }
+    const compareSolution = () => {
+        const userInput = document.querySelector('textarea');
+        if (userInput !== null) {
+            let userInputText = userInput.value;
+            
+        }
+    }
+
     return (
+        <>
         <section className="coding-problem">
             <p>{randomProblem}</p>
             <label>Type your answer below:</label>
-            <input type="text" />
-            <button>Feed the code</button>
+            <textarea
+            onChange={readUserInput()}
+            cols="30"
+            rows="10"
+            ></textarea>
+            <button onClick={compareSolution()}>Feed the code</button>
         </section>
+        <section className="solution-result">
+            <p>{result}</p>
+        </section>
+        </>
     )
 }
 
