24d47590222ace5f15a0933dbd1a3b6445d5e3f3
yablony <yablonskaya.yuliya@gmail.com>
2023-06-25 14:48:46 +0930

    Egg Selection page finished, added styling and ability to save the difficulty

--- src/components/Pages/EggSelection.js
+++ src/components/Pages/EggSelection.js
@@ -1,28 +1,61 @@
 import Spline from '@splinetool/react-spline';
 import { Button } from "@mui/material";
 import { useNavigate } from 'react-router-dom';
+import '../Layout/EggSelection.scss';
+import { useState } from 'react';
 
 function EggSelection() {
     const navigate = useNavigate();
+    const [difficultyLevel, setDifficultyLevel] = useState('');
+
+    const updateDifficultyLevel = difficulty => {
+        setDifficultyLevel(difficulty);
+    }
 
     return(
         <div className="eggs">
             <h2>Select your egg</h2>
-            <Spline scene="https://prod.spline.design/SsTNodEztniOUOvz/scene.splinecode" /> 
+            <p>The egg determines the <b>difficulty level</b> of the game</p>
+
+            <section className="eggs-display">
+                <div className="egg">
+                    <Spline 
+                    onClick={() => updateDifficultyLevel('easy')}
+                    className="easy"
+                    scene="https://prod.spline.design/niTRfOHUaCaNPIaq/scene.splinecode" /> 
+                    <h4>NO SAUCE</h4>
+                </div>
+
+                <div className="egg">
+                    <Spline 
+                    onClick={() => updateDifficultyLevel('medium')}
+                    className="medium"
+                    scene="https://prod.spline.design/niTRfOHUaCaNPIaq/scene.splinecode" /> 
+                    <h4>MILD</h4>
+                </div>
+
+                <div className="egg">
+                    <Spline 
+                    onClick={() => updateDifficultyLevel('hard')}
+                    className="hard"
+                    scene="https://prod.spline.design/niTRfOHUaCaNPIaq/scene.splinecode" /> 
+                    <h4>SPICY</h4>
+                </div>
+            </section>
+
             <nav>
                 <Button
                 variant="contained"
                 onClick={() => navigate('/')}
                 >
-                    Back to main page
+                    ← to main page
                 </Button>
                 <Button
                 variant="contained"
                 onClick={() => navigate('/pet')}
                 >
-                    CONTINUE
+                    <b>CONTINUE →</b>
                 </Button>
-
             </nav>
         </div>
     )
--- src/components/Pages/Pet.js
+++ src/components/Pages/Pet.js
@@ -1,6 +1,37 @@
+import difficultyLevel from './EggSelection';
+import { Button } from "@mui/material";
+import { useNavigate } from 'react-router-dom';
+import Spline from '@splinetool/react-spline';
+
+// 2. Add a text bubble
+
 function Pet () {
+    const navigate = useNavigate();
+    console.log(difficultyLevel)
     return(
-        <h1>Shrimpy boy</h1>
+        <div className="pet">
+                <h1>Shrimpy boi</h1>
+
+                <section className="shrimp">
+                <Spline scene="https://prod.spline.design/lQ1KYGirfkeHohde/scene.splinecode" />
+                <h1>🦐</h1>
+                </section>
+
+                <nav>
+                <Button
+                variant="contained"
+                onClick={() => navigate('/eggs')}
+                >
+                    ← to eggs
+                </Button>
+                <Button
+                variant="contained"
+                onClick={() => navigate('/feed')}
+                >
+                    <b>FEED</b>
+                </Button>
+            </nav>
+        </div>
     )
 }
 
