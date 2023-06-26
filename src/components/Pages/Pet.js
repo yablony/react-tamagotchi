32c49bdee2974cd00e27e2b6de5d725650515b38
yablony <yablonskaya.yuliya@gmail.com>
2023-06-26 11:24:31 +0930

    Created a pop-up window to feed the shrimp

--- src/components/Pages/Pet.js
+++ src/components/Pages/Pet.js
@@ -1,12 +1,24 @@
 import difficultyLevel from './EggSelection';
-import { Button } from "@mui/material";
+import { Button, Backdrop, CircularProgress } from "@mui/material";
 import { useNavigate } from 'react-router-dom';
 import Spline from '@splinetool/react-spline';
+import { useState } from 'react';
 import '../Layout/Pet.scss';
 
 function Pet () {
     const navigate = useNavigate();
-    console.log(difficultyLevel)
+    console.log(difficultyLevel);
+
+    const [open, setOpen] = useState(false);
+    
+    const handleClose = () => {
+      setOpen(false);
+    };
+
+    const handleOpen = () => {
+      setOpen(true);
+    };
+
     return(
         <div className="pet">
                 <h1>Shrimpy boi</h1>
@@ -25,10 +37,29 @@ function Pet () {
                 </Button>
                 <Button
                 variant="contained"
-                onClick={() => navigate('/coding-problem')}
+                onClick={handleOpen}
                 >
                     <b>FEED</b>
                 </Button>
+                <Backdrop
+                    sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+                    open={open}
+                    onClick={handleClose}
+                >
+                    <div className="pop-up">
+                        <h1>Feed your shrimp</h1>
+                        <p>
+                            You rcreature feeds on good code.<br/>
+                            Complete a challenge and watch it grow!<br/>
+                        </p>
+                        <Button
+                            variant="contained"
+                            onClick={() => navigate('/coding-problem')}
+                            >
+                                START CHALLENGE
+                        </Button>
+                    </div>
+                </Backdrop>
             </nav>
         </div>
     )
